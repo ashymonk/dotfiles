e3734e176e5d2b6019c0e485949d0b65b11a618d
monkfish <46420556+monkfish1994@users.noreply.github.com>
2019-02-28 15:12:01 +0900

    add bashrc

--- dotfiles_symlink.sh
+++ dotfiles_symlink.sh
@@ -3,5 +3,10 @@ set -eu
 
 DOTFILES_DIR=$(cd $(dirname $0); pwd)
 
-ln -sf ${DOTFILES_DIR}/.vimrc ~/.vimrc
+[ ! -f ~/.bash_profile.old ] && [ -f ~/.bash_profile ] && mv ~/.bash_profile ~/.bash_profile.old
+[ ! -f ~/.bashrc.old ] && [ -f ~/.bashrc ] && mv ~/.bashrc ~/.bashrc.old
+[ ! -f ~/.vimrc.old ] && [ -f ~/.vimrc ] && mv ~/.vimrc ~/.vimrc.old
+
 ln -sf ${DOTFILES_DIR}/.bash_profile ~/.bash_profile
+ln -sf ${DOTFILES_DIR}/.bashrc ~/.bashrc
+ln -sf ${DOTFILES_DIR}/.vimrc ~/.vimrc
