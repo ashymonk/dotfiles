c44c62da230bec45d7e5e24204a2bb760d2f2bfa
ashymonk <46420556+ashymonk@users.noreply.github.com>
2019-03-01 16:07:56 +0900

    update dotfiles_symlink.sh
    change link proc to internal function.

--- dotfiles_symlink.sh
+++ dotfiles_symlink.sh
@@ -1,6 +1,8 @@
 #! /bin/sh
 set -eu
 
+DOTFILES_DIR=$(cd $(dirname $0); pwd)
+
 filebackup()
 {
 	target=${HOME}/$1
@@ -8,12 +10,18 @@ filebackup()
 	[ -f ${target} ] && mv ${target} ${target}.old
 }
 
-DOTFILES_DIR=$(cd $(dirname $0); pwd)
+filelink()
+{
+	srcfile=${DOTFILES_DIR}/$1
+	destfile=${HOME}/$1
+	ln -sf ${srcfile} ${destfile}
+}
 
 filebackup .bash_profile
 filebackup .bashrc
 filebackup .vimrc
 
-ln -sf ${DOTFILES_DIR}/.bash_profile ~/.bash_profile
-ln -sf ${DOTFILES_DIR}/.bashrc ~/.bashrc
-ln -sf ${DOTFILES_DIR}/.vimrc ~/.vimrc
+filelink .bash_profile
+filelink .bashrc
+filelink .vimrc
+
