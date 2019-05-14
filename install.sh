02bceba095935bd0d9f30bc7a4f9c5461bd6b085
monkfish <46420556+monkfish1994@users.noreply.github.com>
2019-05-14 18:06:48 +0900

    update install script

--- install.sh
+++ install.sh
@@ -5,8 +5,22 @@ TARGET_DIR=$HOME
 
 cd $(dirname $0)
 
+. sh/.profile
+
+set -e
+
+mkdir -p $XDG_CONFIG_HOME
+mkdir -p $XDG_CACHE_HOME
+mkdir -p $XDG_DATA_HOME
+
 for package in $(ls -d */)
 do
 	stow -t $TARGET_DIR -R $package
 done
+
+echo ************************************
+echo * dotfiles installation succeeded. *
+echo ************************************
+
+bash --login
 
