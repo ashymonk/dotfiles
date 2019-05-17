25d35048aedc7236e8378882e9cfaa6bac403303
ashymonk <46420556+ashymonk@users.noreply.github.com>
2019-05-17 22:01:16 +0900

    update install shell

--- install.sh
+++ install.sh
@@ -7,8 +7,6 @@ cd $(dirname $0)
 
 . sh/.profile
 
-set -e
-
 mkdir -p $XDG_CONFIG_HOME
 mkdir -p $XDG_CACHE_HOME
 mkdir -p $XDG_DATA_HOME
@@ -16,11 +14,17 @@ mkdir -p $XDG_DATA_HOME
 for package in $(ls -d */)
 do
 	stow -t $TARGET_DIR -R $package
+
+	if [ $? -ne 0 ]; then
+		echo "stowing $package failed."
+		exit 1
+	fi
 done
 
-echo ************************************
-echo * dotfiles installation succeeded. *
-echo ************************************
+echo "************************************"
+echo "* dotfiles installation succeeded. *"
+echo "************************************"
 
 bash --login
 
+exit 0
