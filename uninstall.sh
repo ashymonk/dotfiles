051e8b91a6d15ea4f233138587028dc0badf523f
ashymonk <46420556+ashymonk@users.noreply.github.com>
2019-08-27 18:37:42 +0900

    update install script

--- uninstall.sh
+++ uninstall.sh
@@ -6,16 +6,16 @@ TARGET_DIR=$HOME
 
 for package in $(ls -d */)
 do
-	stow -t $TARGET_DIR -D $package
+	stow -D -t $TARGET_DIR $package
 	if [ $? -ne 0 ]; then
 		echo "unstowing $package failed."
 		exit 1
 	fi
 done
 
-echo "**********************"
+echo "************************"
 echo "* uninstalled dotfiles *"
-echo "**********************"
+echo "************************"
 
 bash -li
 
