49129e7f877630aa675c8672afebdf19e1b165f8
ashymonk <46420556+ashymonk@users.noreply.github.com>
2024-06-16 22:42:38 +0900

    Update bash.
    [bash] Display exit status in prompt if error.

--- bash/.config/bash/rc.d/10_option.bash
+++ bash/.config/bash/rc.d/10_option.bash
@@ -31,15 +31,21 @@ shopt -s autocd
 # make less more friendly for non-text input files, see lesspipe(1)
 [ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
 
+# print exit status if error
+function error_status() {
+    status=$?
+    [ $status -ne 0 ] && echo "($status) "
+}
+
 # set variable identifying the chroot you work in (used in the prompt below)
 if [ -z "${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
     debian_chroot=$(cat /etc/debian_chroot)
 fi
 
 if [ "$__color_prompt" = yes ]; then
-    PS1='\n${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w$(__git_ps1 " (%s)")\n\[\033[00m\]\$ '
+    PS1='\n${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]: \[\033[01;34m\]\w$(__git_ps1 " (%s)")\n\[\033[00m\]$(error_status)\$ '
 else
-    PS1='\n${debian_chroot:+($debian_chroot)}\u@\h:\w$(__git_ps1 " (%s)")\n\$ '
+    PS1='\n${debian_chroot:+($debian_chroot)}\u@\h: \w$(__git_ps1 " (%s)")\n$(error_status)\$ '
 fi
 
 # If this is an xterm set the title to user@host:dir
