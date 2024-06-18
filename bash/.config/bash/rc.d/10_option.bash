60a59f9597e5b14a21c420595f4d51186387646b
ashymonk <46420556+ashymonk@users.noreply.github.com>
2024-06-19 00:06:35 +0900

    Update bash.
    [bash] Split prompt settings.

--- bash/.config/bash/rc.d/10_option.bash
+++ bash/.config/bash/rc.d/10_option.bash
@@ -30,33 +30,3 @@ shopt -s autocd
 
 # make less more friendly for non-text input files, see lesspipe(1)
 [ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
-
-# print exit status if error
-function error_status() {
-    status=$?
-    [ $status -ne 0 ] && echo "($status)"
-}
-
-# set variable identifying the chroot you work in (used in the prompt below)
-if [ -z "${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
-    debian_chroot=$(cat /etc/debian_chroot)
-fi
-
-if [ "$__color_prompt" = yes ]; then
-    PS1='\n${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]: \[\033[01;34m\]\w`__git_ps1 " (%s)"`\n\[\033[00m\]$(error_status)\$ '
-else
-    PS1='\n${debian_chroot:+($debian_chroot)}\u@\h: \w`__git_ps1 " (%s)"`\n$(error_status)\$ '
-fi
-
-# If this is an xterm set the title to user@host:dir
-case "$TERM" in
-    xterm*|rxvt*)
-        PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
-        ;;
-esac
-
-if [ "$__color_prompt" = yes ]; then
-    [ -x /usr/bin/dircolors ] && test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
-    # colored GCC warnings and errors
-    #export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
-fi
---
+++ bash/.config/bash/rc.d/15_prompt.bash
@@ -0,0 +1,35 @@
+# sourced by ~/.bashrc
+
+# print exit status if error
+function exit_status() {
+    status=$?
+    if [ $status -ne 0 ]; then
+        printf "(%3d)" $status
+    else
+        echo "---->"
+    fi
+}
+
+# set variable identifying the chroot you work in (used in the prompt below)
+if [ -z "${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
+    debian_chroot=$(cat /etc/debian_chroot)
+fi
+
+if [ "$__color_prompt" = yes ]; then
+    PS1='\n${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]: \[\033[01;34m\]\w`__git_ps1 " (%s)"`\n\[\033[00m\]$(exit_status) \$ '
+else
+    PS1='\n${debian_chroot:+($debian_chroot)}\u@\h: \w`__git_ps1 " (%s)"`\n$(exit_status) \$ '
+fi
+
+# If this is an xterm set the title to user@host:dir
+case "$TERM" in
+    xterm*|rxvt*)
+        PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
+        ;;
+esac
+
+if [ "$__color_prompt" = yes ]; then
+    [ -x /usr/bin/dircolors ] && test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+    # colored GCC warnings and errors
+    export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
+fi
